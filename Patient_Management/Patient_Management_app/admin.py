c285c552dc244fa2aa754fee9c173a732311c3ec
sharmanitesh884 <53546266+sharmanitesh884@users.noreply.github.com>
2020-06-25 21:47:47 +0530

    Add files via upload

---
+++ Patient_Management/Patient_Management_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from Patient_Management_app.models import UserProfileInfo, User
+# Register your models here.
+admin.site.register(UserProfileInfo)
---
+++ Patient_Management/Patient_Management_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PatientManagementAppConfig(AppConfig):
+    name = 'Patient_Management_app'
